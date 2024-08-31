5331c124ef2ca91a3875f8ef1b69c974d0121a7c
TAHIR WAQAS <bc240213710twa@vu.edu.pk>
2024-08-31 02:19:30 -0700

    Update app.js

--- first/app.js
+++ first/app.js
@@ -1,2 +1,21 @@
 console.log("hello mai fir a gya");
 
+let name = "sahil";
+let age = 20;
+let totalPrice = 100;
+
+console.log(name);
+
+const student = {
+  name: "TAHIR",
+  age: 20,
+  program: "BSCS",
+  Cgpa: 2.88,
+};
+
+student["age"] = student["age"] + 1;
+student["name"] = "Tahir Waqas";
+
+console.log(student, typeof student);
+console.log(student["name"]);
+console.log(student.age);
